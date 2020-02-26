5a59f74de1928c43a3141838f91c9a0de3c26e50
Praveen Kumar Thirupathi <praveen.thirupathi@publicissapient.com>
2020-02-26 18:55:58 +0530

    feat(task-4): added proxy

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -2,21 +2,28 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  **/
-import { Server } from 'hapi';
+import { Server } from '@hapi/hapi';
+
+import * as H2o2 from '@hapi/h2o2';
 
 const init = async () => {
   const server = new Server({
     port: 3333,
     host: 'localhost'
   });
+  await server.register(H2o2);
 
   server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return {
-        hello: 'world'
-      };
+    method: '*',
+    path: '/{path*}',
+    handler: {
+      proxy: {
+        host: 'sandbox.iexapis.com',
+        port: 443,
+        protocol: 'https',
+        passThrough: true,
+        redirects: 5
+      }
     }
   });
 
