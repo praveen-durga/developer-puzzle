4fe5f6ab2d1bbf6e68314dcda3cefd8971ff1a0d
Praveen Kumar Thirupathi <praveen.thirupathi@publicissapient.com>
2020-02-27 22:42:10 +0530

    feat(task-3): filtered date range for all data

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,29 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy {
+  private quotesSubscription: Subscription;
+  private filteredQuotes;
+  private quotes$ = this.priceQuery.priceQueries$;
+  
   stockPickerForm: FormGroup;
   period: string;
   maxDate: Date = new Date();
   toMinDate: Date;
-
-  quotes$ = this.priceQuery.priceQueries$;
+  filteredQuotes$ = new Subject<any>();
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
     this.stockPickerForm = fb.group({
       symbol: [null, Validators.required],
       from: [null, Validators.required],
-      to: [null, Validators.required],
+      to: [null, Validators.required]
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.quotesSubscription = this.quotes$.subscribe((data: any[]) => {
+      if (data) {
+        this.filteredQuotes = data.filter(quote => {
+          const quoteDate = new Date(quote[0]);
+          return (
+            quoteDate.getTime() >= this.fromDate.value.getTime() &&
+            quoteDate.getTime() <= ( this.toDate.value.getTime() + ((1000 * 3600 * 24)))
+          );
+        });
+        if (this.filteredQuotes.length > 0) {
+          this.filteredQuotes$.next(this.filteredQuotes);
+        }
+      }
+    });
+  }
 
   get symbol() {
     return this.stockPickerForm.get('symbol');
@@ -49,9 +68,15 @@ export class StocksComponent implements OnInit {
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
-      const symbol  = this.symbol.value;
-      const period = (this.toDate.value.getTime() - this.fromDate.value.getTime()) / (1000 * 3600 * 24);
-      this.priceQuery.fetchQuote(symbol, `${period + 1}d`);
+      const symbol = this.symbol.value;
+      const period = 'max';
+      this.priceQuery.fetchQuote(symbol, period);
     }
+  }
+  ngOnDestroy() {
+    if (this.quotesSubscription) {
+      this.quotesSubscription.unsubscribe();
+    }
+    this.filteredQuotes$.unsubscribe();
   }
 }
